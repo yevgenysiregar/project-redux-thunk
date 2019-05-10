0aad80e0df1923f684ff8bbb7fc7760dd7712e5d
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-10 15:14:44 +0700

    Edit code adding mapStateToProps

--- src/components/List.js
+++ src/components/List.js
@@ -3,16 +3,26 @@ import { connect } from "react-redux";
 import { fetchData } from "../actions/todoAction";
 
 class List extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.fetchData();
+  }
   render() {
     return (
       <div>
         <h1>Todo List</h1>
+        {this.props.todoList.map((todo, index) => (
+          <div key={index}>{todo.description}</div>
+        ))}
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  todoList: state.todo.todoList
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchData }
 )(List);
